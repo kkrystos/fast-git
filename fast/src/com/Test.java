9a3c8cb57d30a62c1795ae81dd22da29e83a305d
kkrystos <kkrystos@gmail.com>
2014-11-14 22:18:31 +0100

    delete from for alien

--- fast/src/com/Test.java
+++ fast/src/com/Test.java
@@ -14,16 +14,7 @@ public class Test {
 
 		for (int i = 0; i < 10; i++) {
 			List<String> ls = new ArrayList<String>();
-			ls.add("jeden");
-			ls.add("dwa");
-			ls.add("trzy");
-			ls.add("cztery");
-			ls.add("piêæ");
-			ls.add("szeœæ");		
-			ls.add("siedem"); 
-			ls.add("osiem"); 	
-			ls.add("dziewiec"); 		
-			ls.add("dziesiec"); 		
+			
 			listaList.add(ls);
 		}
 
